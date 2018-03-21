45dcdb0797c084d3068dedb39a0d0e11b163312d
Justin Phillips <euangelion3o9@gmail.com>
2018-03-21 18:55:42 -0400

    Added CI config, swapped testing framework.

---
+++ src/AbstractorModule/test_abstractor.py
@@ -0,0 +1,23 @@
+'''
+Abstractor integration tests
+'''
+import pytest
+from .abstractor import get_abstraction
+from .abstractions import ABSTRACTIONS
+
+
+def test_valid_name_returns_valid_name():
+    assert get_abstraction('Jack') == ABSTRACTIONS['PERSON']
+
+
+def test_valid_company_returns_valid_company():
+    assert get_abstraction('IBM') == ABSTRACTIONS['ORG']
+
+
+def test_dollar_value_returns_valid_currency():
+    assert get_abstraction('$5.00') == ABSTRACTIONS['MONEY']
+
+
+def test_invalid_input(self):
+    with pytest.raises(Exception):
+        get_abstraction({})
