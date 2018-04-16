d7853273e26f69f0ced87f692bec5d65782d767d
Gabriel Perez Clark <gaboperezclark@gmail.com>
2018-04-16 14:38:09 -0400

    Updated abstractor tests

--- src/abstractor_module/test_abstractor.py
+++ src/abstractor_module/test_abstractor.py
@@ -6,13 +6,17 @@ from .abstractor import Abstractor
 from .abstractions import ABSTRACTIONS
 
 
-def test_valid_name_returns_valid_name():
-    assert Abstractor().get_abstraction('Jack') == ABSTRACTIONS['FIRST_NAME']
+def test_first_name_returns_valid_first_name():
+    assert Abstractor().get_abstraction('First name Joahsna') == ABSTRACTIONS['FIRST_NAME']
 
+def test_last_name_returns_valid_last_name():
+    assert Abstractor().get_abstraction('last name smith') == ABSTRACTIONS['LAST_NAME']
 
-def test_valid_company_returns_valid_company():
-    assert Abstractor().get_abstraction('Striker') == ABSTRACTIONS['LAST_NAME']
+def test_valid_email_returns_valid_email():
+    assert Abstractor().get_abstraction('email Joahsna@gmail.com') == ABSTRACTIONS['EMAIL']
 
+def test_invalid_email_returns_none():
+    assert Abstractor().get_abstraction('email qq') == None
 
 def test_invalid_input():
     with pytest.raises(Exception):
