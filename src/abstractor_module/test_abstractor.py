afab08742fa06d94e1e0656f6a948852fc1d2c5b
Justin Phillips <euangelion3o9@gmail.com>
2018-04-20 13:05:08 -0400

    More tests and data.

--- src/abstractor_module/test_abstractor.py
+++ src/abstractor_module/test_abstractor.py
@@ -10,13 +10,49 @@ def test_first_name_returns_valid_first_name():
     assert Abstractor().get_abstraction('First name Joahsna') == ABSTRACTIONS['FIRST_NAME']
 
 def test_last_name_returns_valid_last_name():
-    assert Abstractor().get_abstraction('last name smith') == ABSTRACTIONS['LAST_NAME']
+    assert Abstractor().get_abstraction('last name Smith') == ABSTRACTIONS['LAST_NAME']
 
 def test_valid_email_returns_valid_email():
     assert Abstractor().get_abstraction('email Joahsna@gmail.com') == ABSTRACTIONS['EMAIL']
 
+def test_first_name2_returns_valid_first_name():
+    assert Abstractor().get_abstraction('First name* William') == ABSTRACTIONS['FIRST_NAME']
+
+def test_last_name2_returns_valid_last_name():
+    assert Abstractor().get_abstraction('Sur name Jones') == ABSTRACTIONS['LAST_NAME']
+
+def test_valid_email2_returns_valid_email():
+    assert Abstractor().get_abstraction('Email Joahs2462na@gmail.com') == ABSTRACTIONS['EMAIL']
+
 def test_invalid_email_returns_none():
     assert Abstractor().get_abstraction('email qq') == None
+
+def test_invalid_first_name_returns_none():
+    assert Abstractor().get_abstraction('First name car') == None
+
+def test_invalid_last_name_returns_none():
+    assert Abstractor().get_abstraction('last name promise') == None
+
+def test_invalid_email2_returns_none():
+    assert Abstractor().get_abstraction('email @gmail.com') == None
+
+def test_invalid_email3_returns_none():
+    assert Abstractor().get_abstraction('email qq') == None
+
+def test_invalid_email4_returns_none():
+    assert Abstractor().get_abstraction('Email -1.0') == None
+
+def test_invalid_first2_name_returns_none():
+    assert Abstractor().get_abstraction('First name 3.3') == None
+
+def test_invalid_last2_name_returns_none():
+    assert Abstractor().get_abstraction('Last name NY') == None
+
+def test_invalid_email5_returns_none():
+    assert Abstractor().get_abstraction('email lol.example.com') == None
+
+def test_invalid_email6_returns_none():
+    assert Abstractor().get_abstraction('Email: what@e') == None
 
 def test_invalid_input():
     with pytest.raises(Exception):
